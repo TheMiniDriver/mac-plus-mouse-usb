9dd220c17b3caf5e874d5bf60c2b624021fd33f5
Bradley Van Aardt <bradley@indri.co.za>
2020-06-23 21:15:11 +0200

    refactor some loose variables into structs

--- src/dfu-mouse/dfu-mouse.ino
+++ src/dfu-mouse/dfu-mouse.ino
@@ -4,6 +4,13 @@
    Release into the public domain.
 */
 
+
+#define buttonPin  6     // the number of the mouse button pin
+#define y1Pin  2 
+#define y2Pin  3 
+#define x1Pin  4 
+#define x2Pin  5 
+
 struct {
   uint8_t buttons;
   int8_t x;
@@ -12,15 +19,9 @@ struct {
 } mouseReport;
 
 uint8_t nullReport[4] = { 0, 0, 0, 0 };
-const int buttonPin = 6;     // the number of the mouse button pin
-const int y1Pin = 2; 
-const int y2Pin = 3; 
-const int x1Pin = 4; 
-const int x2Pin = 5; 
-
 
 // =================================================================================
-// Type definition
+// Type definitions
 // =================================================================================
 typedef struct
 {
@@ -28,26 +29,30 @@ typedef struct
   uint8_t index      = 0;
 } ENCODER_;
 
+
+typedef struct
+{
+  uint8_t current = 0; 
+  uint8_t old     = 0; 
+} PINRECORD_; 
+
+
 // =================================================================================
 // Constant definition
 // =================================================================================
 const int8_t lookupTable[] = {0, 1, -1, 0, -1, 0, 0, 1, 1, 0, 0, -1, 0, -1,  1,  0};
 
 // =================================================================================
-// Volatile variables
+// Variables
 // =================================================================================
-volatile ENCODER_ xAxis;
-volatile ENCODER_ yAxis;
+ENCODER_ xAxis;
+ENCODER_ yAxis;
 
-uint8_t x1Old;
-uint8_t x2Old; 
-uint8_t y1Old; 
-uint8_t y2Old; 
+PINRECORD_ x1; 
+PINRECORD_ x2;
 
-uint8_t x1Current;
-uint8_t x2Current; 
-uint8_t y1Current; 
-uint8_t y2Current;
+PINRECORD_ y1; 
+PINRECORD_ y2;
 
 int writeDelayCounter = 0; 
 
@@ -71,27 +76,27 @@ void setup()
 void loop()
 {
     // read the state of the pushbutton value:
-    Read_XYInputs(); 
+    ReadXYInputs(); 
     
-    if (Check_for_Input_Change_X()){
+    if (CheckForInputChange(&x1, &x2)){
         ISR_HANDLER_X();
     }
 
-    if (Check_for_Input_Change_Y()){
+    if (CheckForInputChange(&y1, &y2)){
       ISR_HANDLER_Y();
     }
 
     UpdateOldXYValues(); 
     
     if (writeDelayCounter > 1000){
-      // Serial.print("X: "); 
-      // Serial.print(mouseReport.x); 
-      // Serial.print(" Y: "); 
-      // Serial.println(mouseReport.y);
+      // Want to avoid writing too much as it takes a while and we lose too many encoder increments.
       mouseReport.x = xAxis.coordinate;
       mouseReport.y = yAxis.coordinate;
       mouseReport.buttons = ReadButtonInput(); 
-      Serial.write((uint8_t *)&mouseReport, 4);
+      
+      //Serial.write((uint8_t *)&mouseReport, 4);
+      WriteMouseDataToDebug(); 
+     
       writeDelayCounter = 0; 
       xAxis.coordinate = 0;
       yAxis.coordinate = 0;
@@ -100,43 +105,53 @@ void loop()
 }
 
 
-void ReadButtonInput(){
+// =================================================================================
+// Mouse input reading
+// =================================================================================
+
+uint8_t ReadButtonInput(){
     // The mouse button is active low (ie. when pressed it is GND and will read as 0. So we need to invert it, as the USB driver is High for press. 
     uint8_t buttonState = digitalRead(buttonPin);
     return !buttonState;  
 }
 
-void Read_XYInputs()
+void ReadXYInputs()
 {
-   x1Current =  digitalRead(x1Pin); 
-   x2Current =  digitalRead(x2Pin); 
-   y1Current =  digitalRead(y1Pin); 
-   y2Current =  digitalRead(y2Pin); 
+   x1.current =  digitalRead(x1Pin); 
+   x2.current =  digitalRead(x2Pin); 
+   y1.current =  digitalRead(y1Pin); 
+   y2.current =  digitalRead(y2Pin); 
 }
 
 void UpdateOldXYValues(){
-  x1Old = x1Current; 
-  x2Old = x2Current;
-  y1Old = y1Current;
-  y2Old = y2Current;
+  x1.old = x1.current; 
+  x2.old = x2.current;
+  y1.old = y1.current;
+  y2.old = y2.current;
 }
 
-bool Check_for_Input_Change_X(){
-  return ((x1Old != x1Current) || (x2Old != x2Current)); 
+bool CheckForInputChange(PINRECORD_ *rec1, PINRECORD_ *rec2){
+  return ((rec1->old != rec1->current) || (rec2->old != rec2->current)); 
 }
 
-bool Check_for_Input_Change_Y(){
-  return ((y1Old != y1Current) || (y2Old != y2Current)); 
-}
+//bool Check_for_Input_Change_X(){
+//  return ((x1.old != x1.current) || (x2.old != x2.current)); 
+//}
+//
+//bool Check_for_Input_Change_Y(){
+//  return ((y1.old != y1.current) || (y2.old != y2.current)); 
+//}
 
 // =================================================================================
-// Interrupt handlers
+// Encoder Lookups 
+// This was originally on interrupt handlers, but on the UNO there are not enough
+// interrupts (only 2, and we need 4). 
 // =================================================================================
 void ISR_HANDLER_X()
 {
   // Build the LUT index from previous and new data
-  uint8_t xInput = x2Current << 1; 
-  xInput += x1Current;  
+  uint8_t xInput = x2.current << 1; 
+  xInput += x1.current;  
   
   xAxis.index       = (xAxis.index << 2) | (xInput);
   xAxis.coordinate += lookupTable[xAxis.index & 0b1111];
@@ -145,9 +160,23 @@ void ISR_HANDLER_X()
 void ISR_HANDLER_Y()
 {
   // Build the LUT index from previous and new data
-  uint8_t yInput = y2Current << 1; 
-  yInput += y1Current;
+  uint8_t yInput = y2.current << 1; 
+  yInput += y1.current;
   
   yAxis.index       = (yAxis.index << 2) | (yInput);
   yAxis.coordinate += lookupTable[yAxis.index & 0b1111];
+}
+
+// =================================================================================
+// Debug helpers
+// =================================================================================
+
+void WriteMouseDataToDebug(){
+    Serial.print("X: "); 
+    Serial.print(mouseReport.x); 
+    Serial.print(" Y: "); 
+    Serial.print(mouseReport.y);
+    Serial.print(" Button: "); 
+    Serial.println(mouseReport.buttons); 
+
 }
